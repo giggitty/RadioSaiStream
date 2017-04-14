53e2d528136cf8bd33c9e6366604fb3fcd5a9992
giggitty <ananthd@microsoft.com>
2017-04-13 19:51:38 -0700

    Create audioAssets.js

---
+++ js/audioAssets.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title' : 'Bhajan Stream',
+        'url'   : 'http://stream.radiosai.net:8000/'
+    },
+    {
+        'title' : 'Discource Stream',
+        'url'   : 'http://stream.radiosai.net:8008/'
+    },
+    {
+        'title' : 'Telugu Stream',
+        'url'   : 'http://stream.radiosai.net:8020/'
+    },
+    {
+        'title': 'America Stream',
+        'url': 'http://stream.radiosai.net:8006/'
+    }
+];
+
+module.exports = audioData;
