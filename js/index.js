3a094b3d02ff3cba0bec35605c5ff30a6c357de3
giggitty <ananthd@microsoft.com>
2017-04-13 19:53:04 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var constants = require('./constants');
+var stateHandlers = require('./stateHandlers');
+var audioEventHandlers = require('./audioEventHandlers');
+
+exports.handler = function(event, context, callback){
+    try 
+    {
+        var alexa = Alexa.handler(event, context);
+        alexa.appId = constants.appId;
+
+        if (event.session.application.applicationId !== alexa.appId)
+        {
+            context.fail("Invalid Application ID");
+        }
+
+        alexa.dynamoDBTableName = constants.dynamoDBTableName;
+
+        alexa.registerHandlers(
+            stateHandlers.startModeIntentHandlers,
+            stateHandlers.playModeIntentHandlers,
+            stateHandlers.remoteControllerHandlers,
+            stateHandlers.resumeDecisionModeIntentHandlers,
+            audioEventHandlers
+        );
+        alexa.execute();
+    }
+    catch (e)
+    {
+        content.fail("Exception: " + e);
+    }
+    
+};
